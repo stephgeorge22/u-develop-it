376ba01c48a3a2b3d0a200817d5820ff96c8dc76
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-10 16:35:29 -0400

    database calls

--- server.js
+++ server.js
@@ -2,10 +2,28 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+  
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
